bd2afb4d3c86ba55a5953ba7b71a87186feb5325
Vasyl Gryshchenko <vasyl.gryshchenko@dxc.com>
2022-05-03 09:54:13 +0300

    Add test_char function

--- test.c
+++ test.c
@@ -9,3 +9,10 @@ int test(void *data)
         return *((int *)data);
     return *(int *)0;
 }
+
+char *test_char(void *data)
+{
+    if((char*) data)
+        return (char*) data;
+    return (char*) 0;
+}
